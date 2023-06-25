f1c1a36e136b6a347e11fae88180f136a2190454
luiscorreia79 <136075367+luiscorreia79@users.noreply.github.com>
2023-06-25 09:26:52 +0200

    Update preparation.sh

--- scripts/preparation.sh
+++ scripts/preparation.sh
@@ -5,4 +5,4 @@ unzip cuad-training/data.zip -d cuad-data/
 mkdir cuad-models
 curl https://zenodo.org/record/4599830/files/roberta-base.zip?download=1 --output cuad-models/roberta-base.zip
 unzip cuad-models/roberta-base.zip -d cuad-models/
-python -m pip install --upgrade pip'
+python -m pip install --upgrade pip
